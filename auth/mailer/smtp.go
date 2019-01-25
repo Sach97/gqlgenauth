e95406dcca37cd0d7e921c3468d970033ecdb0c5
Sach97 <sacha.arbonel@hotmail.fr>
2019-01-25 15:36:20 +0100

    getToken

--- auth/mailer/smtp.go
+++ auth/mailer/smtp.go
@@ -9,27 +9,28 @@ type Config struct {
 	string identity,
 	string username,
 	string password,
-	string host
+	string host,
+	string address,
 }
 
-func (c *Config) SendMail(to []string , msg []byte) {
+func (c *Config) SendMail(from string,to []string , msg []byte) {
 	auth := smtp.PlainAuth(c.identity, c.username, c.password, c.host) //smtp.PlainAuth("", "user@example.com", "password", "mail.example.com")
 
 	// Connect to the server, authenticate, set the sender and recipient,
 
 	// and send the email all in one step.
 
-	to := []string{"recipient@example.net"}
+	// to := []string{"recipient@example.net"}
 
-	msg := []byte("To: recipient@example.net\r\n" +
+	// msg := []byte("To: recipient@example.net\r\n" +
 
-		"Subject: discount Gophers!\r\n" +
+	// 	"Subject: discount Gophers!\r\n" +
 
-		"\r\n" +
+	// 	"\r\n" +
 
-		"This is the email body.\r\n")
+	// 	"This is the email body.\r\n")
 
-	err := smtp.SendMail("mail.example.com:25", auth, "sender@example.org", to, msg)
+	err := smtp.SendMail(c.address, auth, from, to, msg)
 
 	if err != nil {
 
--- auth/main.go
+++ auth/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Sach97/gqlgenauth/auth/utils"
 )
 
@@ -8,12 +10,15 @@ func main() {
 
 	client := utils.New()
 
-	// token, err := client.GenerateString()
-	// if err != nil {
-	// 	fmt.Println(token)
-	// }
+	token, err := client.GenerateString()
+	if err != nil {
+		panic(err)
+	}
 
-	token := "abed1971-3fd9-4319-a2d4-d6fc4e5943bd"
-	utils.GetToken(token)
+	value, err := client.GetToken(token)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(value)
 
 }
