33c56e9fcb739300774e2bac06af82b7960b8ae4
Sach97 <sacha.arbonel@hotmail.fr>
2019-03-05 18:05:19 +0100

    expose a function to instantiate all ninshoo objects

--- go.mod
+++ go.mod
@@ -3,16 +3,21 @@ module github.com/Sach97/ninshoo
 go 1.12
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-chi/chi v4.0.2+incompatible // indirect
 	github.com/go-chi/jwtauth v3.3.0+incompatible
 	github.com/go-redis/redis v6.15.2+incompatible
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/lib/pq v1.0.0
+	github.com/onsi/ginkgo v1.7.0 // indirect
+	github.com/onsi/gomega v1.4.3 // indirect
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
 	github.com/rs/xid v1.2.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.3.1
 	golang.org/x/crypto v0.0.0-20190228161510-8dd112bcdc25
+	google.golang.org/appengine v1.4.0 // indirect
 )
---
+++ ninshoo.go
@@ -0,0 +1,51 @@
+package ninshoo
+
+import (
+	"github.com/Sach97/ninshoo/builder"
+	"github.com/Sach97/ninshoo/context"
+	"github.com/Sach97/ninshoo/db"
+	"github.com/Sach97/ninshoo/deeplinker"
+	"github.com/Sach97/ninshoo/jwt"
+	"github.com/Sach97/ninshoo/mailer"
+	"github.com/Sach97/ninshoo/tokenizer"
+	"github.com/Sach97/ninshoo/user"
+	"github.com/Sach97/ninshoo/utils"
+)
+
+func NewNinshoo() *user.Service {
+	// // Context Stuffs
+	cfg := context.LoadConfig(".")
+
+	// // Token stuffs
+	RedisClient := tokenizer.NewRedisClient(cfg)
+	t := tokenizer.Tokenizer{RedisClient}
+
+	// // Mail stuffs
+	m := mailer.NewMailer(cfg)
+
+	// // Firebase STUFFS
+	d := deeplinker.NewFireBaseClient(cfg)
+
+	//DB STUFFS
+	sql := db.Strategy(db.DriverSQL{Name: "postgres"})
+
+	s, err := sql.OpenDB(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	//Log stuffs
+	l := utils.NewLoggerService(cfg)
+
+	//JWT stuffs
+	auth := jwt.NewAuthService(cfg)
+
+	//Message service stuffs
+	msg := context.NewMessageService(cfg)
+
+	//Builder service stuffs
+	b := builder.NewBuilderService(cfg)
+
+	// User service stuffs
+	return user.NewUserService(msg, s, l, auth, &t, m, d, b)
+}
